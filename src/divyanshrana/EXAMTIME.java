527cd280b795296d91e366020404a7b2a4542232
officialdivyanshrana <divyans3@shernet.sheridancollege.ca>
2023-06-20 16:10:44 -0400

    commit file EXAMTIME

---
+++ src/divyanshrana/EXAMTIME.java
@@ -0,0 +1,15 @@
+package divyanshrana;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *THIS IS FOR THE EXAM PREPRESION!
+ * @author rdivy
+ */
+public class EXAMTIME {
+    
+}
